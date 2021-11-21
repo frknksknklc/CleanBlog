1df776e21f5dfdc41f4533f0e61f71b0b2db8009
Furkan Keskinkilic <furkankeskinkilic97@gmail.com>
2021-11-21 23:05:37 +0300

    template engine middleware and static files added

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
 const app = express();
 
+
+//Template engine
+app.set('view engine', 'ejs');
+//Middlewares
+app.use(express.static('public'));
+
+
 app.get('/', function (req, res) {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render("index")
+});
+app.get('/about', function (req, res) {
+  res.render("about")
+});
+app.get('/add_post', function (req, res) {
+  res.render("add_post")
+});
+app.get('/post', function (req, res) {
+  res.render("post")
 });
 const port = 3000;
 app.listen(port, () => {
